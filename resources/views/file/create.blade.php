6575795c05ebfdead5c31760a0ff2b8f72ee724a
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-02-06 15:50:16 +0330

    design upload form!

--- resources/views/file/create.blade.php
+++ resources/views/file/create.blade.php
@@ -3,6 +3,9 @@
 @section('content')
     <div class="row my-4">
         <div class="col-lg-8 col-md-6 mb-md-0 mb-4">
+            <div>
+{{--                @include('partials.alerts')--}}
+            </div>
             <div class="card">
                 <div class="card-header pb-0">
                     <div class="row mb-3">
@@ -12,7 +15,32 @@
                     </div>
                 </div>
                 <div class="card-body p-0 pb-2">
+                    <form action="# " method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group">
+                            <div class="mb-3 m-4">
+                                <lable class="col-form-label" for="customFile">فایل خود را انتخاب کنید</lable>
+                                <input type="file" name="file" class="input-group" id="customFile">
 
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="mb-3 m-4">
+                                <lable class="col-form-label" for="is-private">فایل خود را انتخاب کنید</lable>
+                                <input type="checkbox" name="is-private" class="input-group" id="is-private">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="text-center m-4">
+                                <button type="submit" class="btn btn-primary">آپلود فایل</button>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+{{--                            @include('partials.validation')--}}
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
