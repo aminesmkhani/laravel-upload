c7f6fab42b8510546ccb0a1328278b9982b9cdf7
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-02-09 16:53:49 +0330

    define deleteFile function in StorageManager!

--- app/Services/Uploader/StorageManager.php
+++ app/Services/Uploader/StorageManager.php
@@ -31,6 +31,11 @@ class StorageManager
         return $this->disk($isPrivate)->download($this->directoryPrefix($type,$name));
     }
 
+    public function deleteFile(string $name,string $type, bool $isPrivate)
+    {
+        return $this->disk($isPrivate)->delete($this->directoryPrefix($type, $name));
+    }
+
     private function directoryPrefix($type, $name)
     {
         return $type . DIRECTORY_SEPARATOR . $name;
