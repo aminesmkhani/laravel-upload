3beb6a47d66305cfd99cfaa43f43ab386caeba4a
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-02-09 12:47:04 +0330

    add new service for FFMpeegService & add to config service & .env!

---
+++ app/Services/Uploader/FFMpegService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services\Uploader;
+
+use FFMpeg\FFProbe;
+
+class FFMpegService
+{
+    private $ffprobe;
+
+    public function __construct()
+    {
+        $this->ffprobe = FFProbe::create([
+            'ffprobe.binaries'  => config('services.ffmpeg.ffprobe_path')
+        ]);
+    }
+}
--- config/services.php
+++ config/services.php
@@ -30,4 +30,8 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'ffmpeg' => [
+        'ffprobe_path'  => env('FFPROBE_PATH')
+    ]
+
 ];
